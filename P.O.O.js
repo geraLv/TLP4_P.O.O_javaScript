42c0e738182bb7cfcf21403fef1e610fc6bbaaba
Gerardo Vega <gerardoluisvega@gmail.com>
2025-08-12 11:06:20 -0300

    fix: :memo: Punto 2 de la investigación

--- P.O.O.js
+++ P.O.O.js
@@ -1,25 +1,26 @@
 function CuentaBancaria(titular, saldoInicial) {
   this.titular = titular;
   this.saldoInicial = saldoInicial;
-
-  this.depositar = function (monto) {
-    return (this.saldoInicial = this.saldoInicial + monto);
-  };
-
-  this.extraer = function (monto) {
-    return (this.saldoInicial = this.saldoInicial - monto);
-  };
-
-  this.consultarSaldo = function () {
-    return console.log("Saldo actual: ", this.saldoInicial);
-  };
 }
+CuentaBancaria.prototype.depositar = function (monto) {
+  this.saldoInicial += monto;
+  console.log("Monto depositado correctamente");
+};
+
+CuentaBancaria.prototype.retirar = function (monto) {
+  this.saldoInicial -= monto;
+  console.log("Monto extraido correctamente");
+};
+
+CuentaBancaria.prototype.consultarSaldo = function () {
+  console.log("Saldo actual: ", this.saldoInicial);
+};
 
 const juan = new CuentaBancaria("juan", 500);
 
 juan.consultarSaldo();
 
-juan.extraer(209);
+juan.retirar(209);
 juan.consultarSaldo();
 juan.depositar(400);
 juan.consultarSaldo();
